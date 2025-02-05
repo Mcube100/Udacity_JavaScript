9c3562a19a3fd5314e846b103ea5017d39808396
Melaku <melaku10500@gmail.com>
2025-02-05 12:04:24 +0300

    while loop

---
+++ while_loop/fizz_buzz.js
@@ -0,0 +1,21 @@
+let x = 1;
+
+while (x <= 100){
+    if(x%3===0 && x%5===0){
+        console.log("FizzBuzz");
+    }
+    else if (x%3===0){
+        console.log("Fizz");
+        
+    }
+    else if(x%5===0){
+        console.log("Buzz");
+    }
+   
+    else if(x%3!=0 || x%5 != 0)
+        console.log(x);
+    else
+    console.log("Invalid Number");
+
+x = x + 1;
+}
---
+++ while_loop/intial_while_loop.js
@@ -0,0 +1,20 @@
+//while loop repeat an action some number of times.
+
+let x = 1; // when to start
+
+while (x <= 10){ // when to stop
+    console.log( x + " practice needed to be professional Developer.");
+    x = x + 1; // next item
+}
+
+
+
+
+
+let y = 10;
+
+while (y <= 25){
+    console.log("Printing out x = " + y);
+    y = y + 2;
+}
+    
